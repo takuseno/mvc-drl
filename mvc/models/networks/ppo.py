12949c0fd49128baf24dcd627615954c5c8578a3
takuseno <takuma.seno@gmail.com>
2019-01-07 23:59:18 +0900

    Refactor PPONetwork

--- mvc/models/networks/ppo.py
+++ mvc/models/networks/ppo.py
@@ -9,17 +9,20 @@ class PPONetwork(BaseNetwork):
                  num_envs,
                  num_actions,
                  time_horizon,
+                 lr,
                  grad_clip,
-                 lr):
-        self._build(function, state_shape, num_envs, num_actions,
-                    time_horizon, grad_clip, lr)
+                 value_factor,
+                 entropy_factor):
+        self._build(
+            function, state_shape, num_envs,
+            num_actions, time_horizon, grad_clip, lr)
 
     def _infer(self, **kwargs):
         feed_dict = {
             self.step_obs_ph: kwargs['obs_t'],
         }
         sess = tf.get_default_session()
-        ops = [self.action, self.log_policy, self.step_value]
+        ops = [self.action, self.log_policy, self.value]
         return ActionOutput(*sess.run(ops, feed_dict=feed_dict))
 
     def _update(self, **kwargs):
@@ -39,14 +42,17 @@ class PPONetwork(BaseNetwork):
                num_envs,
                num_actions,
                time_horizon,
+               lr,
                grad_clip,
-               lr):
-        batch_size = nenvs * time_horizon
+               value_factor,
+               entropy_factor):
+
+        batch_size = num_envs * time_horizon
+
         with tf.variable_scope('ppo', reuse=tf.AUTO_REUSE):
             # placeholers
             step_obs_ph = self.step_obs_ph = tf.placeholder(
-                tf.float32, [nenvs] + state_shape, name='step_obs')
-            # for recurrent version extension
+                tf.float32, [num_envs] + state_shape, name='step_obs')
             train_obs_ph = self.train_obs_ph = tf.placeholder(
                 tf.float32, [batch_size] + state_shape, name='train_obs')
             returns_ph = self.returns_t = tf.placeholder(
@@ -59,33 +65,27 @@ class PPONetwork(BaseNetwork):
                 tf.float32, [batch_size], name='old_log_prob')
 
             # network outputs for inference
-            step_dist, self.step_value = function(step_obs_ph)
+            step_dist, step_values = function(step_obs_ph)
             # network outputs for training
-            train_dist, train_value = model(train_obs_ph)
+            train_dist, train_values = function(train_obs_ph)
+
+            # prepare for loss calculation
+            advantages = tf.reshape(advantages_ph, [-1, 1])
+            returns = tf.reshape(returns_ph, [-1, 1])
+            log_probs = train_dist.log_prob(actions_ph)
+
+            # individual loss
+            value_loss = self._build_value_loss(train_values, returns,
+                                                value_factor)
+            policy_loss = self._build_policy_loss(log_probs, old_log_probs_ph,
+                                                  advantages, epsilon)
+            entropy_loss = self._build_entropy_loss(train_dist, entropy_factor)
+            # final loss
+            self.loss = value_loss + policy_loss + entropy_loss
 
             # network weights
             network_vars = tf.get_collection(
                 tf.GraphKeys.TRAINABLE_VARIABLES, scope)
-
-            # loss
-            advantages = tf.reshape(advantages_ph, [-1, 1])
-            returns = tf.reshape(returns_ph, [-1, 1])
-            with tf.variable_scope('value_loss'):
-                value_loss = tf.reduce_mean(tf.square(returns - train_value))
-                value_loss *= value_factor
-            with tf.variable_scope('entropy'):
-                entropy = tf.reduce_mean(train_dist.entropy())
-                entropy *= entropy_factor
-            with tf.variable_scope('policy_loss'):
-                log_prob = train_dist.log_prob(actions_ph)
-                ratio = tf.exp(log_prob - old_log_probs_ph)
-                ratio = tf.reduce_mean(ratio, axis=1, keep_dims=True)
-                surr1 = ratio * advantages
-                surr2 = tf.clip_by_value(
-                    ratio, 1.0 - epsilon, 1.0 + epsilon) * advantages
-                surr = tf.minimum(surr1, surr2)
-                policy_loss = tf.reduce_mean(surr)
-            self.loss = value_loss - policy_loss - entropy
 
             # gradients
             gradients = tf.gradients(self.loss, network_vars)
@@ -98,6 +98,28 @@ class PPONetwork(BaseNetwork):
             # action
             self.action = step_dist.sample(1)[0]
             self.log_policy = step_dist.log_prob(action)
+            self.value = tf.reshape(step_values, [-1])
+
+    def _build_value_loss(self, values, returns, value_factor):
+        with tf.variable_scope('value_loss'):
+            loss = tf.reduce_mean((returns - values) ** 2)
+            return value_factor * value_loss
+
+    def _build_entropy_loss(self, dist, entropy_factor):
+        with tf.variable_scope('entropy'):
+            entropy = -tf.reduce_mean(dist.entropy())
+            return entropy_factor * entropy
+
+    def _build_policy_loss(self, log_probs, old_log_probs, advantages, epsilon):
+        with tf.variable_scope('policy_loss'):
+            ratio = tf.exp(log_probs - old_log_probs)
+            ratio = tf.reduce_mean(ratio, axis=1, keep_dims=True)
+            surr1 = ratio * advantages
+            surr2 = tf.clip_by_value(
+                ratio, 1.0 - epsilon, 1.0 + epsilon) * advantages
+            surr = tf.minimum(surr1, surr2)
+            policy_loss = -tf.reduce_mean(surr)
+        return policy_loss
 
     def _infer_arguments(self):
         return ['obs_t']
