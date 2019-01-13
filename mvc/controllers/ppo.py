77a46be54991d9d24087dcab402ff2b8ace9d10b
takuseno <takuma.seno@gmail.com>
2019-01-13 15:54:56 +0900

    Implement epoch loop at PPOController

--- mvc/controllers/ppo.py
+++ mvc/controllers/ppo.py
@@ -7,21 +7,15 @@ from mvc.controllers.base_controller import BaseController
 from mvc.preprocess import compute_returns, compute_gae
 
 
-def shuffle_batch(batch, size):
-    indices = np.random.permutation(np.arange(size))
-    # check first not to change original data in the error case
-    for key in batch.keys():
-        assert batch[key].shape[0] == size
-    for key in batch.keys():
-        batch[key] = batch[key][indices]
-    return batch
-
 class PPOController(BaseController):
     def __init__(self,
                  network,
                  rollout,
                  metrics,
+                 num_envs,
                  time_horizon,
+                 epoch,
+                 batch_size,
                  gamma,
                  lam,
                  log_interval=None):
@@ -32,7 +26,10 @@ class PPOController(BaseController):
         self.network = network
         self.rollout = rollout
         self.metrics = metrics
+        self.num_envs = num_envs
         self.time_horizon = time_horizon
+        self.epoch = epoch
+        self.batch_size = batch_size
         self.gamma = gamma
         self.lam = lam
         self.log_interval = time_horizon if not log_interval else log_interval
@@ -49,8 +46,8 @@ class PPOController(BaseController):
                          output.value, output.log_prob, done)
 
         # record metrics
-        self.metrics.add('step', obs.shape[0])
-        for i in range(obs.shape[0]):
+        self.metrics.add('step', self.num_envs)
+        for i in range(self.num_envs):
             if done[i]:
                 self.metrics.add('reward', info[i]['reward'])
 
@@ -63,16 +60,23 @@ class PPOController(BaseController):
         assert self.should_update()
 
         # create batch from stored trajectories
-        batch = shuffle_batch(self._batch(), self.time_horizon)
+        batches = self._batches()
+
         # flush stored trajectories
         self.rollout.flush()
+
         # update parameter
-        loss = self.network.update(**batch)
+        losses = []
+        for _ in range(self.epoch):
+            for batch in batches:
+                loss = self.network.update(**batch)
+                losses.append(loss)
+        mean_loss = np.mean(losses)
 
         # record metrics
-        self.metrics.add('loss', loss)
+        self.metrics.add('loss', mean_loss)
 
-        return loss
+        return mean_loss
 
     def should_log(self):
         return self.metrics.get('step') % self.log_interval == 0
@@ -85,11 +89,14 @@ class PPOController(BaseController):
     def stop_episode(self, obs, reward, info):
         pass
 
-    def _batch(self):
+    def _batches(self):
         assert self.rollout.size() > 1
 
         trajectory = self.rollout.fetch()
         step_length = self.rollout.size() - 1
+        obs_t = trajectory['obs_t'][:step_length]
+        actions_t = trajectory['actions_t'][:step_length]
+        log_probs_t = trajectory['log_probs_t'][:step_length]
         values_t = trajectory['values_t'][:step_length]
         rewards_tp1 = trajectory['rewards_t'][1:step_length + 1]
         terminals_tp1 = trajectory['terminals_t'][1:step_length + 1]
@@ -100,10 +107,34 @@ class PPOController(BaseController):
         advantages_t = compute_gae(bootstrap_value, rewards_tp1, values_t,
                                    terminals_tp1, self.gamma, self.lam)
 
-        return {
-            'obs_t': trajectory['obs_t'][:step_length],
-            'actions_t': trajectory['actions_t'][:step_length],
-            'log_probs_t': trajectory['log_probs_t'][:step_length],
-            'returns_t': returns_t,
-            'advantages_t': advantages_t
-        }
+        # flatten
+        data_size = self.time_horizon * self.num_envs
+        flat_obs_t = np.reshape(obs_t, (data_size,) + obs_t.shape[2:])
+        flat_actions_t = np.reshape(actions_t, (data_size, -1))
+        flat_log_probs_t = np.reshape(log_probs_t, (data_size, -1))
+        flat_returns_t = np.reshape(returns_t, (-1,))
+        flat_advantages_t = np.reshape(advantages_t, (-1,))
+
+        # shuffle
+        indices = np.random.permutation(np.arange(data_size))
+        shuffled_obs_t = flat_obs_t[indices]
+        shuffled_actions_t = flat_actions_t[indices]
+        shuffled_log_probs_t = flat_log_probs_t[indices]
+        shuffled_returns_t = flat_returns_t[indices]
+        shuffled_advantages_t = flat_advantages_t[indices]
+
+        # create batch data
+        batches = []
+        for i in range(data_size // self.batch_size):
+            start = self.batch_size * i
+            end = self.batch_size * (i + 1)
+            batch = {
+                'obs_t': shuffled_obs_t[start:end],
+                'actions_t': shuffled_actions_t[start:end],
+                'log_probs_t': shuffled_log_probs_t[start:end],
+                'returns_t': shuffled_returns_t[start:end],
+                'advantages_t': shuffled_advantages_t[start:end]
+            }
+            batches.append(batch)
+
+        return batches
--- tests/controllers/test_ppo.py
+++ tests/controllers/test_ppo.py
@@ -6,7 +6,7 @@ import mvc.logger as logger
 
 from unittest.mock import MagicMock, Mock
 from mvc.models.networks.base_network import BaseNetwork
-from mvc.controllers.ppo import PPOController, shuffle_batch
+from mvc.controllers.ppo import PPOController
 from mvc.models.rollout import Rollout
 from mvc.models.metrics import Metrics
 from mvc.preprocess import compute_returns, compute_gae
@@ -61,62 +61,37 @@ def make_inputs():
     info = [{'reward': np.random.random()} for _ in range(4)]
     return obs, reward, done, info
 
-class ShuffleBatchTest(unittest.TestCase):
-    def test_success(self):
-        batch = {
-            'test1': np.random.random((4, 5)),
-            'test2': np.random.random((4, 10)),
-            'test3': np.random.random((4, 15)),
-        }
-        backup = copy.deepcopy(batch)
-        shuffled_batch = shuffle_batch(batch, 4)
-        for key in backup.keys():
-            self.assertFalse(np.all(shuffled_batch[key] == backup[key]))
-
-    def test_with_dimension_error(self):
-        batch = {
-            'test1': np.random.random((5, 5)),
-            'test2': np.random.random((4, 10))
-        }
-        backup = copy.deepcopy(batch)
-        with pytest.raises(AssertionError):
-            shuffle_batch(batch, 4)
-        for key in backup.keys():
-            assert np.all(batch[key] == backup[key])
-
-        batch = {
-            'test1': np.random.random((4, 5)),
-            'test2': np.random.random((5, 10))
-        }
-        backup = copy.deepcopy(batch)
-        with pytest.raises(AssertionError):
-            shuffle_batch(batch, 4)
-        for key in backup.keys():
-            assert np.all(batch[key] == backup[key])
-
 class PPOControllerTest(unittest.TestCase):
     def test_init_success(self):
         network = DummyNetwork()
         rollout = Rollout()
         metrics = DummyMetrics()
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
 
     def test_init_with_error(self):
         with pytest.raises(AssertionError):
             network = 'network'
             rollout = Rollout()
             metrics = DummyMetrics()
-            cotroller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+            controller = PPOController(network, rollout, metrics, num_envs=4,
+                                       time_horizon=128, epoch=4, batch_size=32,
+                                       gamma=0.99, lam=0.9)
         with pytest.raises(AssertionError):
             network = DummyNetwork()
             rollout = 'rollout'
             metrics = DummyMetrics()
-            cotroller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+            controller = PPOController(network, rollout, metrics, num_envs=4,
+                                       time_horizon=128, epoch=4, batch_size=32,
+                                       gamma=0.99, lam=0.9)
         with pytest.raises(AssertionError):
             network = DummyNetwork()
             rollout = Rollout()
             metrics = 'metrics'
-            cotroller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+            controller = PPOController(network, rollout, metrics, num_envs=4,
+                                       time_horizon=128, epoch=4, batch_size=32,
+                                       gamma=0.99, lam=0.9)
 
     def test_step(self):
         rollout = Rollout()
@@ -125,7 +100,9 @@ class PPOControllerTest(unittest.TestCase):
         output = make_output()
         network._infer = MagicMock(return_value=output)
         network._infer_arguments = MagicMock(return_value=['obs_t'])
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
         inputs = make_inputs()
         action = controller.step(*inputs)
 
@@ -145,55 +122,51 @@ class PPOControllerTest(unittest.TestCase):
         output = make_output()
         network._infer = MagicMock(return_value=output)
         network._infer_arguments = MagicMock(return_value=['obs_t'])
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
         inputs = make_inputs()
-        for i in range(20):
+        for i in range(128):
             controller.step(*inputs)
             self.assertFalse(controller.should_update())
         controller.step(*inputs)
         self.assertTrue(controller.should_update())
 
-    def test_batch_success(self):
+    def test_batches(self):
         rollout = Rollout()
         network = DummyNetwork()
         metrics = DummyMetrics()
         output = make_output()
         network._infer_arguments = MagicMock(return_value=['obs_t'])
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
         input_history = []
         output_history = []
-        for i in range(21):
+        for i in range(129):
             inputs = make_inputs()
             network._infer = MagicMock(return_value=output)
             action = controller.step(*inputs)
             input_history.append(inputs)
             output_history.append(output)
 
-        batch = controller._batch()
+        batches = controller._batches()
 
+        assert len(batches) == 128 * 4 // 32
         for key in ['obs_t', 'actions_t', 'log_probs_t', 'returns_t', 'advantages_t']:
-            assert key in batch
-            assert batch[key].shape[0] == 20
-
-        obs_t = np.array([inputs[0] for inputs in input_history])
-        assert np.all(batch['obs_t'] == obs_t[:-1])
-
-        actions_t = np.array([output.action for output in output_history])
-        assert np.all(batch['actions_t'] == actions_t[:-1])
-
-        log_probs_t = np.array([output.log_prob for output in output_history])
-        assert np.all(batch['log_probs_t'] == log_probs_t[:-1])
-
-        rewards_tp1 = np.array([inputs[1] for inputs in input_history])[1:]
-        terminals_tp1 = np.array([inputs[2] for inputs in input_history])[1:]
-        values_t = np.array([output.value for output in output_history])[:-1]
-        bootstrap_value = np.array(output_history[-1].value)
-
-        returns_t = compute_returns(bootstrap_value, rewards_tp1, terminals_tp1, 0.99)
-        assert np.all(returns_t == batch['returns_t'])
-        advantages_t = compute_gae(bootstrap_value, rewards_tp1, values_t,
-                                   terminals_tp1, 0.99, 0.9)
-        assert np.all(advantages_t == batch['advantages_t'])
+            for batch in batches:
+                assert key in batch
+                assert batch[key].shape[0] == 32
+                if key == 'obs_t':
+                    assert batch[key].shape[1:] == inputs[0].shape[1:]
+                elif key == 'actions_t':
+                    assert batch[key].shape[1] == action.shape[1]
+                elif key == 'log_probs_t':
+                    assert batch[key].shape[1] == action.shape[1]
+                elif key == 'returns_t':
+                    assert len(batch[key].shape) == 1
+                elif key == 'advantages_t':
+                    assert len(batch[key].shape) == 1
 
     def test_batch_with_short_trajectory_error(self):
         rollout = Rollout()
@@ -202,11 +175,13 @@ class PPOControllerTest(unittest.TestCase):
         output = make_output()
         network._infer_arguments = MagicMock(return_value=['obs_t'])
         network._infer = MagicMock(return_value=output)
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
         inputs = make_inputs()
         action = controller.step(*inputs)
         with pytest.raises(AssertionError):
-            controller._batch()
+            controller._batches()
 
     def test_update_with_should_update_false(self):
         rollout = Rollout()
@@ -216,7 +191,9 @@ class PPOControllerTest(unittest.TestCase):
         output = make_output()
         network._infer = MagicMock(return_value=output)
         network._infer_arguments = MagicMock(return_value=['obs_t'])
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
         for i in range(20):
             action = controller.step(*inputs)
         with pytest.raises(AssertionError):
@@ -233,10 +210,12 @@ class PPOControllerTest(unittest.TestCase):
         network._infer_arguments = MagicMock(return_value=['obs_t'])
         network._update_arguments = MagicMock(return_value=['obs_t', 'actions_t', 'returns_t', 'advantages_t', 'log_probs_t'])
         network._update = MagicMock(return_value=loss)
-        controller = PPOController(network, rollout, metrics, 20, 0.99, 0.9)
-        for i in range(21):
+        controller = PPOController(network, rollout, metrics, num_envs=4,
+                                   time_horizon=128, epoch=4, batch_size=32,
+                                   gamma=0.99, lam=0.9)
+        for i in range(129):
             action = controller.step(*inputs)
-        
-        self.assertEqual(controller.update(), loss)
-        self.assertEqual(rollout.size(), 0)
-        network._update.assert_called_once()
+
+        assert np.allclose(controller.update(), loss)
+        assert rollout.size() == 0
+        assert network._update.call_count == 128 * 4 * 4 // 32
