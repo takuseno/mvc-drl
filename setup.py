132cb9c25b0341bcce42fc5bf9cde0980474fac0
takuseno <takuma.seno@gmail.com>
2019-02-14 16:32:36 +0900

    Modify version of setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mvc',
-    version='1.0.0',
+    version='1.1.0',
     description='Cleanest Deep Reinforcement Learning Implementation Based on Web MVC',
     long_description=long_description,
     long_description_content_type='text/markdown',
