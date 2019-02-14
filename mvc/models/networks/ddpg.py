443e18a32ddada16859f47f9e41702207f48bca8
takuseno <takuma.seno@gmail.com>
2019-02-14 12:13:00 +0900

    Refactor DDPGNetwork with data class

--- mvc/models/networks/ddpg.py
+++ mvc/models/networks/ddpg.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import tensorflow as tf
 import numpy as np
 
@@ -41,18 +43,14 @@ def build_optim(loss, learning_rate, scope):
     return optimize_expr
 
 
+DDPGNetworkParams = namedtuple(
+    'DDPGNetworkParams', ('fcs', 'concat_index', 'state_shape', 'num_actions',
+                          'gamma', 'tau', 'actor_lr', 'critic_lr'))
+
+
 class DDPGNetwork(BaseNetwork):
-    def __init__(self,
-                 fcs,
-                 concat_index,
-                 state_shape,
-                 num_actions,
-                 gamma,
-                 tau,
-                 actor_lr,
-                 critic_lr):
-        self._build(fcs, concat_index, state_shape, num_actions,
-                    gamma, tau, actor_lr, critic_lr)
+    def __init__(self, params):
+        self._build(params)
 
     def _infer(self, **kwargs):
         feed_dict = {
@@ -89,75 +87,68 @@ class DDPGNetwork(BaseNetwork):
 
         return critic_loss, actor_loss
 
-    def _build(self,
-               fcs,
-               concat_index,
-               state_shape,
-               num_actions,
-               gamma,
-               tau,
-               actor_lr,
-               critic_lr):
+    def _build(self, params):
         with tf.variable_scope('ddpg', reuse=tf.AUTO_REUSE):
             # placeholder
-            obs_t_ph = self.obs_t_ph = tf.placeholder(
-                tf.float32, [None] + list(state_shape), name='obs_t')
-            actions_t_ph = self.actions_t_ph = tf.placeholder(
-                tf.float32, [None, num_actions], name='actions_t')
-            rewards_tp1_ph = self.rewards_tp1_ph = tf.placeholder(
+            self.obs_t_ph = tf.placeholder(
+                tf.float32, [None] + list(params.state_shape), name='obs_t')
+            self.actions_t_ph = tf.placeholder(
+                tf.float32, [None, params.num_actions], name='actions_t')
+            self.rewards_tp1_ph = tf.placeholder(
                 tf.float32, [None], name='rewards_tp1')
-            obs_tp1_ph = self.obs_tp1_ph = tf.placeholder(
-                tf.float32, [None] + list(state_shape), name='obs_tp1')
-            dones_tp1_ph = self.dones_tp1_ph = tf.placeholder(
+            self.obs_tp1_ph = tf.placeholder(
+                tf.float32, [None] + list(params.state_shape), name='obs_tp1')
+            self.dones_tp1_ph = tf.placeholder(
                 tf.float32, [None], name='dones_tp1')
 
             last_initializer = tf.random_uniform_initializer(-3e-3, 3e-3)
 
             raw_policy_t = deterministic_policy_function(
-                fcs, obs_t_ph, num_actions, tf.nn.tanh, w_init=initializer,
-                last_w_init=last_initializer, last_b_init=last_initializer,
-                scope='actor')
+                params.fcs, self.obs_t_ph, params.num_actions, tf.nn.tanh,
+                w_init=initializer, last_w_init=last_initializer,
+                last_b_init=last_initializer, scope='actor')
             policy_t = tf.nn.tanh(raw_policy_t)
             raw_policy_tp1 = deterministic_policy_function(
-                fcs, obs_tp1_ph, num_actions, tf.nn.tanh, w_init=initializer,
-                last_w_init=last_initializer, last_b_init=last_initializer,
-                scope='target_actor')
+                params.fcs, self.obs_tp1_ph, params.num_actions, tf.nn.tanh,
+                w_init=initializer, last_w_init=last_initializer,
+                last_b_init=last_initializer, scope='target_actor')
             policy_tp1 = tf.nn.tanh(raw_policy_tp1)
 
             q_t = q_function(
-                fcs, obs_t_ph, actions_t_ph, concat_index, tf.nn.tanh,
-                w_init=initializer, last_w_init=last_initializer,
-                last_b_init=last_initializer, scope='critic')
+                params.fcs, self.obs_t_ph, self.actions_t_ph,
+                params.concat_index, tf.nn.tanh, w_init=initializer,
+                last_w_init=last_initializer, last_b_init=last_initializer,
+                scope='critic')
             q_t_with_actor = q_function(
-                fcs, obs_t_ph, policy_t, concat_index, tf.nn.tanh,
-                w_init=initializer, last_w_init=last_initializer,
+                params.fcs, self.obs_t_ph, policy_t, params.concat_index,
+                tf.nn.tanh, w_init=initializer, last_w_init=last_initializer,
                 last_b_init=last_initializer, scope='critic')
             q_tp1 = q_function(
-                fcs, obs_tp1_ph, policy_tp1, concat_index, tf.nn.tanh,
-                w_init=initializer, last_w_init=last_initializer,
+                params.fcs, self.obs_tp1_ph, policy_tp1, params.concat_index,
+                tf.nn.tanh, w_init=initializer, last_w_init=last_initializer,
                 last_b_init=last_initializer, scope='target_critic')
 
             # prepare for loss calculation
-            rewards_tp1 = tf.reshape(rewards_tp1_ph, [-1, 1])
-            dones_tp1 = tf.reshape(dones_tp1_ph, [-1, 1])
+            rewards_tp1 = tf.reshape(self.rewards_tp1_ph, [-1, 1])
+            dones_tp1 = tf.reshape(self.dones_tp1_ph, [-1, 1])
 
             # critic loss
             self.critic_loss = build_critic_loss(q_t, rewards_tp1, q_tp1,
-                                                 dones_tp1, gamma)
+                                                 dones_tp1, params.gamma)
             # actor loss
             self.actor_loss = -tf.reduce_mean(q_t_with_actor)
 
             # target update
             self.update_target_critic = build_target_update(
-                'ddpg/critic', 'ddpg/target_critic', tau)
+                'ddpg/critic', 'ddpg/target_critic', params.tau)
             self.update_target_actor = build_target_update(
-                'ddpg/actor', 'ddpg/target_actor', tau)
+                'ddpg/actor', 'ddpg/target_actor', params.tau)
 
             # optimization
-            self.critic_optimize_expr = build_optim(self.critic_loss,
-                                                    critic_lr, 'ddpg/critic')
-            self.actor_optimize_expr = build_optim(self.actor_loss,
-                                                   actor_lr, 'ddpg/actor')
+            self.critic_optimize_expr = build_optim(
+                self.critic_loss, params.critic_lr, 'ddpg/critic')
+            self.actor_optimize_expr = build_optim(
+                self.actor_loss, params.actor_lr, 'ddpg/actor')
 
             # action
             self.action = policy_t
