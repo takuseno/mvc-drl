e8353f1f60ffa2b9a5ef061eb1ee0af21b8842aa
takuma.seno <takuma.seno@U05385noMacBook-Pro.local>
2019-02-14 11:45:26 +0900

    Refactor SACNetwork with dataclass

--- mvc/models/networks/sac.py
+++ mvc/models/networks/sac.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import tensorflow as tf
 import numpy as np
 
@@ -68,20 +70,19 @@ def build_policy_reg(dist, scale):
     return scale * (mean_loss + logstd_loss)
 
 
+SACNetworkParams = namedtuple(
+    'SACNetworkParams', ('fcs', 'concat_index', 'state_shape', 'num_actions',
+                         'gamma', 'tau', 'pi_lr', 'q_lr', 'v_lr', 'reg'))
+
+
+# initialzier
+ZEROS_INIT = tf.zeros_initializer()
+XAVIER_INIT = tf.contrib.layers.xavier_initializer()
+
+
 class SACNetwork(BaseNetwork):
-    def __init__(self,
-                 fcs,
-                 concat_index,
-                 state_shape,
-                 num_actions,
-                 gamma,
-                 tau,
-                 pi_lr,
-                 q_lr,
-                 v_lr,
-                 reg):
-        self._build(fcs, concat_index, state_shape, num_actions,
-                    gamma, tau, pi_lr, q_lr, v_lr, reg)
+    def __init__(self, params):
+        self._build(params)
 
     def _infer(self, **kwargs):
         sess = tf.get_default_session()
@@ -127,93 +128,85 @@ class SACNetwork(BaseNetwork):
 
         return v_loss, (q1_loss, q2_loss), pi_loss
 
-    def _build(self,
-               fcs,
-               concat_index,
-               state_shape,
-               num_actions,
-               gamma,
-               tau,
-               pi_lr,
-               q_lr,
-               v_lr,
-               reg):
+    def _build(self, params):
         with tf.variable_scope('sac'):
-            obs_t_ph = self.obs_t_ph = tf.placeholder(
-                tf.float32, (None,) + state_shape, name='obs_t')
-            actions_t_ph = self.actions_t_ph = tf.placeholder(
-                tf.float32, (None, num_actions), name='actions_t')
-            rewards_tp1_ph = self.rewards_tp1_ph = tf.placeholder(
+            self.obs_t_ph = tf.placeholder(
+                tf.float32, (None,) + params.state_shape, name='obs_t')
+            self.actions_t_ph = tf.placeholder(
+                tf.float32, (None, params.num_actions), name='actions_t')
+            self.rewards_tp1_ph = tf.placeholder(
                 tf.float32, (None,), name='rewards_tp1')
-            obs_tp1_ph = self.obs_tp1_ph = tf.placeholder(
-                tf.float32, (None,) + state_shape, name='obs_tp1')
-            dones_tp1_ph = self.dones_tp1_ph = tf.placeholder(
+            self.obs_tp1_ph = tf.placeholder(
+                tf.float32, (None,) + params.state_shape, name='obs_tp1')
+            self.dones_tp1_ph = tf.placeholder(
                 tf.float32, (None,), name='dones_tp1')
 
-            # initialzier
-            zeros_init = tf.zeros_initializer()
-            xavier_init = tf.contrib.layers.xavier_initializer()
-
             # policy function
-            pi_t = stochastic_policy_function(fcs, obs_t_ph, num_actions,
+            pi_t = stochastic_policy_function(params.fcs, self.obs_t_ph,
+                                              params.num_actions,
                                               tf.nn.relu, share=True,
-                                              w_init=xavier_init,
-                                              last_w_init=xavier_init,
-                                              last_b_init=xavier_init,
+                                              w_init=XAVIER_INIT,
+                                              last_w_init=XAVIER_INIT,
+                                              last_b_init=XAVIER_INIT,
                                               scope='pi')
             squashed_action_t, log_prob_t = squash_action(pi_t)
 
             # value function
             v_t = value_function(
-                fcs, obs_t_ph, tf.nn.relu, xavier_init,
-                xavier_init, zeros_init, scope='v')
+                params.fcs, self.obs_t_ph, tf.nn.relu, XAVIER_INIT,
+                XAVIER_INIT, ZEROS_INIT, scope='v')
             # target value function
             v_tp1 = value_function(
-                fcs, obs_tp1_ph, tf.nn.relu, xavier_init,
-                xavier_init, zeros_init, scope='target_v')
+                params.fcs, self.obs_tp1_ph, tf.nn.relu, XAVIER_INIT,
+                XAVIER_INIT, ZEROS_INIT, scope='target_v')
 
             # two q functions
-            q1_t_with_pi = q_function(fcs, obs_t_ph, squashed_action_t,
-                                      concat_index, tf.nn.relu, xavier_init,
-                                      xavier_init, zeros_init, scope='q1')
-            q1_t = q_function(fcs, obs_t_ph, actions_t_ph, concat_index,
-                              tf.nn.relu, xavier_init, xavier_init,
-                              zeros_init, scope='q1')
-            q2_t_with_pi = q_function(fcs, obs_t_ph, squashed_action_t,
-                                      concat_index, tf.nn.relu, xavier_init,
-                                      xavier_init, zeros_init, scope='q2')
-            q2_t = q_function(fcs, obs_t_ph, actions_t_ph, concat_index,
-                              tf.nn.relu, xavier_init, xavier_init,
-                              zeros_init, scope='q2')
+            q1_t_with_pi = q_function(params.fcs, self.obs_t_ph,
+                                      squashed_action_t, params.concat_index,
+                                      tf.nn.relu, XAVIER_INIT,
+                                      XAVIER_INIT, ZEROS_INIT, scope='q1')
+            q1_t = q_function(params.fcs, self.obs_t_ph, self.actions_t_ph,
+                              params.concat_index, tf.nn.relu, XAVIER_INIT,
+                              XAVIER_INIT, ZEROS_INIT, scope='q1')
+            q2_t_with_pi = q_function(params.fcs, self.obs_t_ph,
+                                      squashed_action_t, params.concat_index,
+                                      tf.nn.relu, XAVIER_INIT,
+                                      XAVIER_INIT, ZEROS_INIT, scope='q2')
+            q2_t = q_function(params.fcs, self.obs_t_ph, self.actions_t_ph,
+                              params.concat_index, tf.nn.relu, XAVIER_INIT,
+                              XAVIER_INIT, ZEROS_INIT, scope='q2')
 
             # prepare for loss
-            rewards_tp1 = tf.reshape(rewards_tp1_ph, [-1, 1])
-            dones_tp1 = tf.reshape(dones_tp1_ph, [-1, 1])
+            rewards_tp1 = tf.reshape(self.rewards_tp1_ph, [-1, 1])
+            dones_tp1 = tf.reshape(self.dones_tp1_ph, [-1, 1])
 
             # value function loss
             self.v_loss = build_v_loss(
                 v_t, q1_t_with_pi, q2_t_with_pi, log_prob_t)
             # q function loss
             self.q1_loss = build_q_loss(
-                q1_t, rewards_tp1, v_tp1, dones_tp1, gamma)
+                q1_t, rewards_tp1, v_tp1, dones_tp1, params.gamma)
             self.q2_loss = build_q_loss(
-                q2_t, rewards_tp1, v_tp1, dones_tp1, gamma)
+                q2_t, rewards_tp1, v_tp1, dones_tp1, params.gamma)
             # policy function loss
             self.pi_loss = build_pi_loss(
                 log_prob_t, q1_t_with_pi, q2_t_with_pi)
             # policy reguralization
-            policy_decay = build_policy_reg(pi_t, reg)
+            policy_decay = build_policy_reg(pi_t, params.reg)
 
             # target update
             self.target_update = build_target_update(
-                'sac/v', 'sac/target_v', tau)
+                'sac/v', 'sac/target_v', params.tau)
 
             # optimization
-            self.v_optimize_expr = build_optim(self.v_loss, v_lr, 'sac/v')
-            self.q1_optimize_expr = build_optim(self.q1_loss, q_lr, 'sac/q1')
-            self.q2_optimize_expr = build_optim(self.q2_loss, q_lr, 'sac/q2')
-            self.pi_optimize_expr = build_optim(self.pi_loss + policy_decay,
-                                                pi_lr, 'sac/pi')
+            self.v_optimize_expr = build_optim(
+                self.v_loss, params.v_lr, 'sac/v')
+            self.q1_optimize_expr = build_optim(
+                self.q1_loss, params.q_lr, 'sac/q1')
+            self.q2_optimize_expr = build_optim(
+                self.q2_loss, params.q_lr, 'sac/q2')
+            self.pi_optimize_expr = build_optim(
+                self.pi_loss + policy_decay, params.pi_lr, 'sac/pi')
 
             # for inference
             self.action = squashed_action_t[0]
