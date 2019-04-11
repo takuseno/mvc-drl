a7d22f826a082a7cd1ba2fa243641a5dcbea979c
takuseno <takuma.seno@gmail.com>
2019-04-12 01:06:30 +0900

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+apt-get update && apt-get install -y --no-install-recommends \
+    build-essential software-properties-common cmake curl python3-dev \
+    x11-apps zlib1g zlib1g-dev graphviz libgl1-mesa-dri libgl1-mesa-glx \
+    libglu1-mesa-dev xvfb x11-utils libasio-dev git pkg-config python3-tk \
+    libsm6 libxext6 libxrender1 git tmux
+
+pip --no-cache-dir install -r requirements.txt
+pip --no-cache-dir install tensorflow-probability-gpu==0.5.0rc0 pybullet
