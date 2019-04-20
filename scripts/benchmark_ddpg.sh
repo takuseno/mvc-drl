c24807e356a2bfb7ddf5c6af011150c36d38907b
takuseno <takuma.seno@gmail.com>
2019-04-20 15:36:06 +0900

    Add log name flags to benchmark scripts

--- scripts/benchmark_ddpg.sh
+++ scripts/benchmark_ddpg.sh
@@ -1,41 +1,41 @@
 # InvertedPendulum
-python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 0
-python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 1
-python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 2
-python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 3
-python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 4
+python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 0 --name pendulum_seed0
+python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 1 --name pendulum_seed1
+python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 2 --name pendulum_seed2
+python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 3 --name pendulum_seed3
+python -m examples.ddpg --env InvertedPendulumBulletEnv-v0 --seed 4 --name pendulum_seed4
 
 # Hopper
-python -m examples.ddpg --env HopperBulletEnv-v0 --seed 0
-python -m examples.ddpg --env HopperBulletEnv-v0 --seed 1
-python -m examples.ddpg --env HopperBulletEnv-v0 --seed 2
-python -m examples.ddpg --env HopperBulletEnv-v0 --seed 3
-python -m examples.ddpg --env HopperBulletEnv-v0 --seed 4
+python -m examples.ddpg --env HopperBulletEnv-v0 --seed 0 --name hopper_seed0
+python -m examples.ddpg --env HopperBulletEnv-v0 --seed 1 --name hopper_seed1
+python -m examples.ddpg --env HopperBulletEnv-v0 --seed 2 --name hopper_seed2
+python -m examples.ddpg --env HopperBulletEnv-v0 --seed 3 --name hopper_seed3
+python -m examples.ddpg --env HopperBulletEnv-v0 --seed 4 --name hopper_seed4
 
 # Walker
-python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 0
-python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 1
-python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 2
-python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 3
-python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 4
+python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 0 --name walker_seed0
+python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 1 --name walker_seed1
+python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 2 --name walker_seed2
+python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 3 --name walker_seed3
+python -m examples.ddpg --env Walker2DBulletEnv-v0 --seed 4 --name walker_seed4
 
 # HalfCheetah
-python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 0
-python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 1
-python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 2
-python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 3
-python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 4
+python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 0 --name cheetah_seed0
+python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 1 --name cheetah_seed1
+python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 2 --name cheetah_seed2
+python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 3 --name cheetah_seed3
+python -m examples.ddpg --env HalfCheetahBulletEnv-v0 --seed 4 --name cheetah_seed4
 
 # Ant
-python -m examples.ddpg --env AntBulletEnv-v0 --seed 0
-python -m examples.ddpg --env AntBulletEnv-v0 --seed 1
-python -m examples.ddpg --env AntBulletEnv-v0 --seed 2
-python -m examples.ddpg --env AntBulletEnv-v0 --seed 3
-python -m examples.ddpg --env AntBulletEnv-v0 --seed 4
+python -m examples.ddpg --env AntBulletEnv-v0 --seed 0 --name ant_seed0
+python -m examples.ddpg --env AntBulletEnv-v0 --seed 1 --name ant_seed1
+python -m examples.ddpg --env AntBulletEnv-v0 --seed 2 --name ant_seed2
+python -m examples.ddpg --env AntBulletEnv-v0 --seed 3 --name ant_seed3
+python -m examples.ddpg --env AntBulletEnv-v0 --seed 4 --name ant_seed4
 
 # Humanoid
-python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 0
-python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 1
-python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 2
-python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 3
-python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 4
+python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 0 --name humanoid_seed0
+python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 1 --name humanoid_seed1
+python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 2 --name humanoid_seed2
+python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 3 --name humanoid_seed3
+python -m examples.ddpg --env HumanoidBulletEnv-v0 --seed 4 --name humanoid_seed4
