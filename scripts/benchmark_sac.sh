03230b76e5c86a61d33d239817331726839a82f6
takuseno <takuma.seno@gmail.com>
2019-04-20 15:46:53 +0900

    Add reward scale to sac benchmark script

--- scripts/benchmark_sac.sh
+++ scripts/benchmark_sac.sh
@@ -1,41 +1,41 @@
 # InvertedPendulum
-python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 0 --name pendulum_seed0
-python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 1 --name pendulum_seed1
-python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 2 --name pendulum_seed2
-python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 3 --name pendulum_seed3
-python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 4 --name pendulum_seed4
+python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 0 --name pendulum_seed0 --reward-scale 5
+python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 1 --name pendulum_seed1 --reward-scale 5
+python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 2 --name pendulum_seed2 --reward-scale 5
+python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 3 --name pendulum_seed3 --reward-scale 5
+python -m examples.sac --env InvertedPendulumBulletEnv-v0 --seed 4 --name pendulum_seed4 --reward-scale 5
 
 # Hopper
-python -m examples.sac --env HopperBulletEnv-v0 --seed 0 --name hopper_seed0
-python -m examples.sac --env HopperBulletEnv-v0 --seed 1 --name hopper_seed1
-python -m examples.sac --env HopperBulletEnv-v0 --seed 2 --name hopper_seed2
-python -m examples.sac --env HopperBulletEnv-v0 --seed 3 --name hopper_seed3
-python -m examples.sac --env HopperBulletEnv-v0 --seed 4 --name hopper_seed4
+python -m examples.sac --env HopperBulletEnv-v0 --seed 0 --name hopper_seed0 --reward-scale 5
+python -m examples.sac --env HopperBulletEnv-v0 --seed 1 --name hopper_seed1 --reward-scale 5
+python -m examples.sac --env HopperBulletEnv-v0 --seed 2 --name hopper_seed2 --reward-scale 5
+python -m examples.sac --env HopperBulletEnv-v0 --seed 3 --name hopper_seed3 --reward-scale 5
+python -m examples.sac --env HopperBulletEnv-v0 --seed 4 --name hopper_seed4 --reward-scale 5
 
 # Walker
-python -m examples.sac --env Walker2DBulletEnv-v0 --seed 0 --name walker_seed0
-python -m examples.sac --env Walker2DBulletEnv-v0 --seed 1 --name walker_seed1
-python -m examples.sac --env Walker2DBulletEnv-v0 --seed 2 --name walker_seed2
-python -m examples.sac --env Walker2DBulletEnv-v0 --seed 3 --name walker_seed3
-python -m examples.sac --env Walker2DBulletEnv-v0 --seed 4 --name walker_seed4
+python -m examples.sac --env Walker2DBulletEnv-v0 --seed 0 --name walker_seed0 --reward-scale 5
+python -m examples.sac --env Walker2DBulletEnv-v0 --seed 1 --name walker_seed1 --reward-scale 5
+python -m examples.sac --env Walker2DBulletEnv-v0 --seed 2 --name walker_seed2 --reward-scale 5
+python -m examples.sac --env Walker2DBulletEnv-v0 --seed 3 --name walker_seed3 --reward-scale 5
+python -m examples.sac --env Walker2DBulletEnv-v0 --seed 4 --name walker_seed4 --reward-scale 5
 
 # HalfCheetah
-python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 0 --name cheetah_seed0
-python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 1 --name cheetah_seed1
-python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 2 --name cheetah_seed2
-python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 3 --name cheetah_seed3
-python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 4 --name cheetah_seed4
+python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 0 --name cheetah_seed0 --reward-scale 5
+python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 1 --name cheetah_seed1 --reward-scale 5
+python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 2 --name cheetah_seed2 --reward-scale 5
+python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 3 --name cheetah_seed3 --reward-scale 5
+python -m examples.sac --env HalfCheetahBulletEnv-v0 --seed 4 --name cheetah_seed4 --reward-scale 5
 
 # Ant
-python -m examples.sac --env AntBulletEnv-v0 --seed 0 --name ant_seed0
-python -m examples.sac --env AntBulletEnv-v0 --seed 1 --name ant_seed1
-python -m examples.sac --env AntBulletEnv-v0 --seed 2 --name ant_seed2
-python -m examples.sac --env AntBulletEnv-v0 --seed 3 --name ant_seed3
-python -m examples.sac --env AntBulletEnv-v0 --seed 4 --name ant_seed4
+python -m examples.sac --env AntBulletEnv-v0 --seed 0 --name ant_seed0 --reward-scale 5
+python -m examples.sac --env AntBulletEnv-v0 --seed 1 --name ant_seed1 --reward-scale 5
+python -m examples.sac --env AntBulletEnv-v0 --seed 2 --name ant_seed2 --reward-scale 5
+python -m examples.sac --env AntBulletEnv-v0 --seed 3 --name ant_seed3 --reward-scale 5
+python -m examples.sac --env AntBulletEnv-v0 --seed 4 --name ant_seed4 --reward-scale 5
 
 # Humanoid
-python -m examples.sac --env HumanoidBulletEnv-v0 --seed 0 --name humanoid_seed0
-python -m examples.sac --env HumanoidBulletEnv-v0 --seed 1 --name humanoid_seed1
-python -m examples.sac --env HumanoidBulletEnv-v0 --seed 2 --name humanoid_seed2
-python -m examples.sac --env HumanoidBulletEnv-v0 --seed 3 --name humanoid_seed3
-python -m examples.sac --env HumanoidBulletEnv-v0 --seed 4 --name humanoid_seed4
+python -m examples.sac --env HumanoidBulletEnv-v0 --seed 0 --name humanoid_seed0 --reward-scale 20
+python -m examples.sac --env HumanoidBulletEnv-v0 --seed 1 --name humanoid_seed1 --reward-scale 20
+python -m examples.sac --env HumanoidBulletEnv-v0 --seed 2 --name humanoid_seed2 --reward-scale 20
+python -m examples.sac --env HumanoidBulletEnv-v0 --seed 3 --name humanoid_seed3 --reward-scale 20
+python -m examples.sac --env HumanoidBulletEnv-v0 --seed 4 --name humanoid_seed4 --reward-scale 20
